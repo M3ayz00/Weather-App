e372ddd332252c07d2d701d6b98e9cc3e34dea30
Mouataz Saadidi <saadidimouataz@gmail.com>
2023-02-16 20:00:22 +0100

    created weather app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  
+  const [data,setData]=useState({});
+  const [location,setLocation]=useState('');
+  const url= `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=b7b31431c67b4014bfa5b1319e23fb94` ;
+  
+  const searchLocation =(event)=> {
+    if(event.key==='Enter'){
+      axios.get(url).then((response)=>{
+        setData(response.data)
+        console.log(response.data)
+      })
+    }
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <div className='search'>
+          <input 
+          type="text"
+          value={location}
+          onChange={event=>setLocation(event.target.value) }
+          onKeyDown={searchLocation}
+          placeholder="Enter Location..."
+          />
+        </div>
+        <div className='top'>
+          <div className='location'>
+            <p>{data.name}</p>
+          </div>
+          <div className='temp'>
+            {data.main ? <h1>{data.main.temp}</h1> : null}
+          </div>
+          <div className='description'>
+            {data.weather ? <p>{data.weather[0].description}</p> : null}
+          </div>
+        </div>
+        <div className='bottom'>
+            <div className='feels'>
+              {data.main? <p>{data.main.feels_like}°F</p> : null}
+            </div>
+            <div className='humidity'>
+              {data.main ? <p>{data.main.humidity}%</p> : null}
+            </div>
+            <div className='wind'>
+              {data.wind ? <p>{data.wind.speed}MPH</p> : null}
+            </div>
+          
+        </div>
+      </div>
     </div>
   );
 }
